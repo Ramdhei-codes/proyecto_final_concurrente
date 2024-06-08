26b2ff6e49ee5abe18704024e862aa2d41276a89
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-06-08 12:06:02 -0500

    manejo de error del secuencial

--- dotplot_secuencial.py
+++ dotplot_secuencial.py
@@ -21,11 +21,15 @@ def generate_dotplot(seq1, seq2, window_size=1):
     seq1_array = np.array(list(seq1))
     seq2_array = np.array(list(seq2))
 
-    for i in tqdm(range(len1 - window_size + 1), desc="Generando dotplot"):
-        sub_seq1 = seq1_array[i:i + window_size]
-        matches = np.where((seq2_array[:len2 - window_size + 1] == sub_seq1[:, None]).all(axis=0))[0]
-        rows.extend([i] * len(matches))
-        cols.extend(matches)
+    try:
+        for i in tqdm(range(len1 - window_size + 1), desc="Generando dotplot"):
+            sub_seq1 = seq1_array[i:i + window_size]
+            matches = np.where((seq2_array[:len2 - window_size + 1] == sub_seq1[:, None]).all(axis=0))[0]
+            rows.extend([i] * len(matches))
+            cols.extend(matches)
+    except MemoryError:
+        print("Error de memoria: No es posible generar el dotplot con las secuencias dadas debido a limitaciones de memoria.")
+        return None
     
     dotplot = coo_matrix((np.ones(len(rows)), (rows, cols)), shape=(len1, len2), dtype=int)
     return dotplot.tocsr()
@@ -44,7 +48,10 @@ def main(file1, file2, output_file, max_length):
     print(f"Longitud de la secuencia 2: {len(seq2)}")
     
     dotplot = generate_dotplot(seq1, seq2)
-    plot_dotplot(dotplot, output_file)
+    if dotplot is not None:
+        plot_dotplot(dotplot, output_file)
+    else:
+        print("No se pudo generar el dotplot debido a un error de memoria.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generar dotplot de dos secuencias FASTA.")
