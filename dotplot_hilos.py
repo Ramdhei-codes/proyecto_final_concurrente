7b3ff0fe649490e97f9052640f45b72565ef0f4a
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-06-08 17:36:27 -0500

    ejecución hilos

--- dotplot_hilos.py
+++ dotplot_hilos.py
@@ -89,4 +89,5 @@ if __name__ == "__main__":
 
     main(args.file1, args.file2, args.output, args.max_length, args.num_threads)
 
-    # ejecutar con:  python .\dotplot_secuencial.py --file1 .\dotplot_files\Salmonella.fna --file2 .\dotplot_files\E_coli.fna --max_length 10000 --output .\dotplot_secuencial.png
+# ejecutar con:  
+# python .\dotplot_secuencial.py --file1 .\dotplot_files\Salmonella.fna --file2 .\dotplot_files\E_coli.fna --max_length 10000 --output .\dotplot_secuencial.png
