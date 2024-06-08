7afd96927eb558777c124ee5a6e7c23dcb5c5ac3
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-06-08 16:13:14 -0500

    menejo de error en mpi4py

--- dotplot_mpi4py.py
+++ dotplot_mpi4py.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy.sparse import coo_matrix, save_npz, load_npz
 from mpi4py import MPI
+import sys
 
 def read_fasta(file_path, max_length=None):
     """Lee una secuencia de un archivo FASTA y devuelve la secuencia."""
@@ -25,11 +26,15 @@ def generate_dotplot_parallel(seq1, seq2, window_size=1, comm=None):
     seq1_array = np.array(list(seq1))
     seq2_array = np.array(list(seq2))
 
-    for i in tqdm(range(rank, len1 - window_size + 1, size), desc=f"Proceso {rank}"):
-        sub_seq1 = seq1_array[i:i + window_size]
-        matches = np.where((seq2_array[:len2 - window_size + 1] == sub_seq1[:, None]).all(axis=0))[0]
-        rows.extend([i] * len(matches))
-        cols.extend(matches)
+    try:
+        for i in tqdm(range(rank, len1 - window_size + 1, size), desc=f"Proceso {rank}"):
+            sub_seq1 = seq1_array[i:i + window_size]
+            matches = np.where((seq2_array[:len2 - window_size + 1] == sub_seq1[:, None]).all(axis=0))[0]
+            rows.extend([i] * len(matches))
+            cols.extend(matches)
+    except MemoryError:
+        print(f"Error de memoria en el proceso {rank}: No es posible generar el dotplot con las secuencias dadas debido a limitaciones de memoria.")
+        comm.Abort()
 
     local_dotplot = coo_matrix((np.ones(len(rows)), (rows, cols)), shape=(len1, len2), dtype=np.uint8)
 
@@ -69,9 +74,13 @@ def main(file1, file2, output_file, max_length):
     seq1 = comm.bcast(seq1, root=0)
     seq2 = comm.bcast(seq2, root=0)
 
-    dotplot = generate_dotplot_parallel(seq1, seq2, comm=comm)
+    try:
+        dotplot = generate_dotplot_parallel(seq1, seq2, comm=comm)
+    except MemoryError:
+        print("Error de memoria durante la ejecución principal.")
+        comm.Abort()
 
-    if rank == 0:
+    if rank == 0 and dotplot is not None:
         plot_dotplot(dotplot, output_file)
 
 if __name__ == "__main__":
@@ -85,5 +94,5 @@ if __name__ == "__main__":
 
     main(args.file1, args.file2, args.output, args.max_length)
 
-# Ejecutar con 4 procesos
-# mpiexec -n 16 python dotplot_mpi4py.py --file1=./dotplot_files/E_coli.fna --file2=./dotplot_files/Salmonella.fna --output=dotplot_mpi.png --max_length=10000
+# Ejecutar con varios procesos
+# mpiexec -n 4 python dotplot_mpi4py.py --file1=./dotplot_files/E_coli.fna --file2=./dotplot_files/Salmonella.fna --output=dotplot_mpi.png --max_length=10000
