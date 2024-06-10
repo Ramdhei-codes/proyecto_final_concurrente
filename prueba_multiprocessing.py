7da83200a681a20fe012bea3410d66eee9c08786
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-06-10 10:54:00 -0500

    tiempo de generación de imagen multiprocessing

--- prueba_multiprocessing.py
+++ prueba_multiprocessing.py
@@ -6,7 +6,7 @@ import argparse
 from Bio import SeqIO
 
 def read_fasta(file_path, max_length=None):
-    """Read a sequence from a FASTA file and return the sequence."""
+    """Lee una secuencia de un archivo FASTA y devuelve la secuencia."""
     for record in SeqIO.parse(file_path, "fasta"):
         sequence = str(record.seq)
         if max_length:
@@ -33,25 +33,35 @@ def parallel_dotplot(Secuencia1, Secuencia2, threads=mp.cpu_count()):
     return np.array(results)
 
 def draw_dotplot(matrix, fig_name='dotplot_multiprocessing.png'):
+    start_time = time.time()  # Tiempo inicial para la generación de la imagen
     plt.figure(figsize=(5, 5))
     plt.imshow(matrix, cmap='Greys', interpolation='none')
     plt.ylabel("Secuencia 1")
     plt.xlabel("Secuencia 2")
     plt.savefig(fig_name)
+    end_time = time.time()  # Tiempo final para la generación de la imagen
+    print(f"Tiempo para generar y guardar la imagen: {end_time - start_time:.2f} segundos")
 
 def main(file1, file2, output_file, max_length, num_processes=mp.cpu_count()):
+    start_time = time.time()  # Tiempo inicial para la ejecución del programa
+
     merged_sequence_1 = read_fasta(file1, max_length)
     merged_sequence_2 = read_fasta(file2, max_length)
 
-    print(len(merged_sequence_1))
-    print(len(merged_sequence_2))
-
-    begin = time.time()
+    print(f"Longitud de la secuencia 1: {len(merged_sequence_1)}")
+    print(f"Longitud de la secuencia 2: {len(merged_sequence_2)}")
+    
+    calc_start_time = time.time()  # Tiempo inicial para los cálculos
     dotplot = parallel_dotplot(merged_sequence_1, merged_sequence_2, num_processes)
+    calc_end_time = time.time()  # Tiempo final para los cálculos
+    
+    print(f"Tiempo de cálculo para generar el dotplot: {calc_end_time - calc_start_time:.2f} segundos")
     print("La matriz de resultado tiene tamaño: ", dotplot.shape)
-    print(f"\n El código se ejecutó en: {time.time() - begin} segundos")
-
+    
     draw_dotplot(dotplot, fig_name=output_file)
+    
+    end_time = time.time()  # Tiempo final para la ejecución del programa
+    print(f"Tiempo total de ejecución del programa: {end_time - start_time:.2f} segundos")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generar dotplot de dos secuencias utilizando multiprocessing")
@@ -64,4 +74,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args.file1, args.file2, args.output, args.max_length, args.num_processes)
+
 #python prueba_multiprocessing.py --file1=./dotplot_files/E_coli.fna --file2=./dotplot_files/Salmonella.fna --output=dotplot_multiprocessing.png --max_length=50000 --num_processes 16
